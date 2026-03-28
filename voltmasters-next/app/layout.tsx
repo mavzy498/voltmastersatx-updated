a4c5cbe3d93eef69d9e4a6fedf1eaa92fb3fd6e6
mavzy498 <maverick@robinsontribe.com>
2026-03-28 14:44:32 -0500

    Update layout.tsx

--- voltmasters-next/app/layout.tsx
+++ voltmasters-next/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({
@@ -35,6 +36,8 @@ export const metadata: Metadata = {
   },
 };
 
+const META_PIXEL_ID = '1577460146887907';
+
 export default function RootLayout({
   children,
 }: {
@@ -42,7 +45,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={'https://www.facebook.com/tr?id=' + META_PIXEL_ID + '&ev=PageView&noscript=1'}
+            alt=""
+          />
+        </noscript>
+      </head>
       <body className={`${inter.variable} font-sans antialiased`}>{children}</body>
     </html>
   );
 }
+
